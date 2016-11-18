87c34176e66c197aa7b17796de352629a8435402
gilgameshkytrooper <leeas@stolaf.edu>
2016-11-18 07:44:12 -0600

    ehh

--- source/test.py
+++ source/test.py
@@ -1,10 +1,17 @@
 # play sound
-import pygame.mixer
-from pygame.mixer import Sound
+# import pygame.mixer
+# from pygame.mixer import Sound
 
 # pygame.mixer.init()
 
-panic_cord = Sound("/home/pi/Projects/AtomicClock/source/samples/panic_cord.wav")
+# panic_cord = Sound("/home/pi/Projects/AtomicClock/source/samples/panic_cord.wav")
 
-while True:
-    panic_cord.play()
+# while True:
+#     panic_cord.play()
+
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load("myFile.wav")
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy() == True:
+    continue
