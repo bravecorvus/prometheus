4c2a17eec2dc39a3f513fafc47e3dd5fb3bc686b
gilgameshskytrooper <leeas@stolaf.edu>
2017-02-12 16:41:58 -0600

    updated node modules

--- source/vibrationtest.py
+++ source/vibrationtest.py
@@ -1,21 +1,41 @@
-# python3 vibrationtest.py
-import RPi.GPIO as GPIO
-import signal
-import sys
-from subprocess import call
+import RPi.GPIO as io
+io.setmode(io.BCM)
 
-GPIO.setmode(GPIO.BCM)
-# GPIO.setup(25, GPIO.IN)
-GPIO.setup(22, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
+in1_pin = 4
+in2_pin = 17
+
+io.setup(in1_pin, io.OUT)
+io.setup(in2_pin, io.OUT)
+
+def set(property, value):
+    try:
+        f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
+        f.write(value)
+        f.close()	
+    except:
+        print("Error writing to: " + property + " value: " + value)
+ 
+set("delayed", "0")
+set("mode", "pwm")
+set("frequency", "500")
+set("active", "1")
+
+def clockwise():
+    io.output(in1_pin, True)    
+    io.output(in2_pin, False)
+
+def counter_clockwise():
+    io.output(in1_pin, False)
+    io.output(in2_pin, True)
+
+clockwise()
 
 while True:
-    userinput = input("on or off\n")
-    if userinput == "on":
-        GPIO.output(22, 1)
-    elif userinput == "off":
-        GPIO.output(22, 0)
-    else:
-        break
-
-GPIO.cleanup()
+    cmd = input("Command, f/r 0..9, E.g. f5 :")
+    direction = cmd[0]
+    if direction == "f":
+        clockwise()
+    else: 
+        counter_clockwise()
+    speed = int(cmd[1]) * 11
+    set("duty", str(speed))
