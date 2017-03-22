52a48242a29b4844eeb48f45406b5dab29548fea
gilgameshkytrooper <leeas@stolaf.edu>
2017-03-21 19:47:39 -0500

    a

--- source/vibrationtest2.py
+++ source/vibrationtest2.py
@@ -2,14 +2,14 @@ import time
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(6, GPIO.OUT)
+GPIO.setup(29, GPIO.OUT) #GPIO 5
+GPIO.setup(31, GPIO.OUT) #GPIO 6
 
-GPIO.output(5, GPIO.HIGH)
-GPIO.outputs(6, GPIO.LOW)
+GPIO.output(29, GPIO.HIGH)
+GPIO.outputs(31, GPIO.LOW)
 
 time.sleep(2)
 
-GPIO.output(5, GPIO.LOW)
-GPIO.outputs(6, GPIO.LOW)
+GPIO.output(29, GPIO.LOW)
+GPIO.outputs(31, GPIO.LOW)
 GPIO.cleanup()
