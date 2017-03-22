dba4a6ace4a8e123436317d37985ad14e06710da
gilgameshskytrooper <leeas@stolaf.edu>
2017-03-21 23:22:16 -0500

    vibrationtest

--- source/vibrationtest.py
+++ source/vibrationtest.py
@@ -6,12 +6,12 @@ GPIO.setup(17, GPIO.OUT) #GPIO 17 EN 1, 2
 GPIO.setup(5, GPIO.OUT) #GPIO 5 INPUT 1
 GPIO.setup(6, GPIO.OUT) #GPIO 6 INPUT 2
 
-#GPIO.output(5, GPIO.HIGH)
-#GPIO.output(6, GPIO.LOW)
-#GPIO.output(17, GPIO.HIGH)
-GPIO.output(5, GPIO.LOW)
-GPIO.output(6, GPIO.HIGH)
+GPIO.output(5, GPIO.HIGH)
+GPIO.output(6, GPIO.LOW)
 GPIO.output(17, GPIO.HIGH)
+#GPIO.output(5, GPIO.LOW)
+#GPIO.output(6, GPIO.HIGH)
+#GPIO.output(17, GPIO.HIGH)
 time.sleep(5)
 
 GPIO.output(17, GPIO.LOW)
