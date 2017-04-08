5e6221570fdcb03562b298ce459462782dae8359
Andrew Lee <leeas@stolaf.edu>
2017-04-08 17:58:51 -0500

    a

--- source/golang/public/js/script.js
+++ source/golang/public/js/script.js
@@ -15,15 +15,15 @@ $.getJSON("/json/alarms.json", function( data ) {
         vibration = data[i].vibration;
         document.getElementById("tit".concat(currentint)).innerHTML = "<h1>"+time+"</h1>";
         if(sound == "on") {
-            $(soundid.concat(currentint)).append('<input id="sound'+currentint+'" name="sound'+currentint+'" type="checkbox" value="on" checked><div class="slider round"></div>')
+            $(soundid.concat(currentint)).append('<input id="sound'+currentint+'" name="sound'+currentint+'" type="checkbox" value="on" checked><div class="slider round" onClick = "document.getElementById("alarm'+currentint+'sound").submit();"></div>')
         } else {
-            $(soundid.concat(currentint)).append('<input id="sound'+currentint+'" name="sound'+currentint+'+" type="checkbox" value="off"><div class="slider round"></div>')
+            $(soundid.concat(currentint)).append('<input id="sound'+currentint+'" name="sound'+currentint+'+" type="checkbox" value="off" onClick = "document.getElementById("alarm'+currentint+'sound").submit();"><div class="slider round"></div>')
         }
         if(vibration == "on") {
-            $(vibid.concat(currentint)).append('<input id="vibration'+currentint+'" name="vibration'+currentint+'" type="checkbox" value="on" checked><div class="slider round"></div>')
+            $(vibid.concat(currentint)).append('<input id="vibration'+currentint+'" name="vibration'+currentint+'" type="checkbox" value="on" checked><div class="slider round" onClick = "document.getElementById("alarm'+currentint+'vibration").submit();"></div>')
 
         } else {
-            $(vibid.concat(currentint)).append('<input id="vibration'+currentint+'" name="vibration'+currentint+'" type="checkbox" value="off"><div class="slider round"></div>')
+            $(vibid.concat(currentint)).append('<input id="vibration'+currentint+'" name="vibration'+currentint+'" type="checkbox" value="off" onClick = "document.getElementById("alarm'+currentint+'vibration").submit();"><div class="slider round"></div>')
         }
     }
 });
