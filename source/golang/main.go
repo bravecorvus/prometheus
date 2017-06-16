800ebb2417acdd23e671b80de55d70853772ffd1
gilgameshkytrooper <leeas@stolaf.edu>
2017-06-16 00:07:15 -0500

    finished version 1

--- source/golang/main.go
+++ source/golang/main.go
@@ -21,6 +21,7 @@ import (
 	"time"
 )
 
+//JSON struct used when reading in the ./public/json/alarms.json to get the alarm configurations when main is first started
 type jsonAlarms struct {
 	JsonName      string `json:"name"`
 	JsonTime      string `json:"time"`
@@ -28,21 +29,28 @@ type jsonAlarms struct {
 	JsonVibration string `json:"vibration"`
 }
 
+//How Prometheus stores the Alarms internally. all 4 alarms are of type "Alarm"
 type Alarm struct {
-	Name             string
-	Alarmtime        string
-	Sound            bool
-	Vibration        bool
-	CurrentlyRunning bool
+	Name             string //Name of the alarm: one of the following: alarm1, alarm2, alarm3, or alarm4
+	Alarmtime        string //Time of the alarm, stored as time; in 24 hour time (e.g. 11:00PM = 23:00).
+	Sound            bool   //Sets whether Prometheus should play sound or not
+	Vibration        bool   //Sets whether Prometheus should vibrate or not
+	CurrentlyRunning bool   //Set to true when alarm is running, false when the user tells it to stop (i.e. Sound/Vibration toggle is turned off for current alarm, it has been more then 10 minutes, or the user pressed the snooze button)
 }
 
+//Declare the alarms
 var Alarm1 = Alarm{}
 var Alarm2 = Alarm{}
 var Alarm3 = Alarm{}
 var Alarm4 = Alarm{}
+
+//Declare the name of the alarm sound stored in ./public/assets/sound_name.extension
 var Soundname string
+
+//Declare the identity of the current wlan0 IP. Used to check if the IP changed.
 var IP, NewIP string
 
+//Taking in the IP as a string as the argument, write the IP address to ./public/json/ip to use when the program is restarted
 func writeIP(arg string) {
 	writebuf := []byte(arg)
 	err := ioutil.WriteFile("./public/json/ip", writebuf, 0644)
@@ -51,6 +59,7 @@ func writeIP(arg string) {
 	}
 }
 
+//Used to execute complex pipes to filter out the wlan0 IP address of the Pi via ifconfig, awk, and cut
 func Execute(output_buffer *bytes.Buffer, stack ...*exec.Cmd) (err error) {
 	var error_buffer bytes.Buffer
 	pipe_stack := make([]*io.PipeWriter, len(stack)-1)
@@ -71,6 +80,7 @@ func Execute(output_buffer *bytes.Buffer, stack ...*exec.Cmd) (err error) {
 	return err
 }
 
+//Used to execute complex pipes to filter out the wlan0 IP address of the Pi via ifconfig, awk, and cut
 func call(stack []*exec.Cmd, pipes []*io.PipeWriter) (err error) {
 	if stack[0].Process == nil {
 		if err = stack[0].Start(); err != nil {
@@ -91,10 +101,12 @@ func call(stack []*exec.Cmd, pipes []*io.PipeWriter) (err error) {
 	return stack[0].Wait()
 }
 
+//Function that returns the current wlan0 address as a string
 func getIP() string {
 	var b bytes.Buffer
 	var str string
 	if err := Execute(&b,
+		//Since piping commands are a bit of a pain, using the above functions call() and Execute(), execute "/sbin/ifconfig wlan0 | grep 'inet addr:' | cut -d -f2 | awk '{print $1}'"
 		exec.Command("/sbin/ifconfig", "wlan0"),
 		exec.Command("grep", "inet addr:"),
 		exec.Command("cut", "-d:", "-f2"),
@@ -112,6 +124,7 @@ func getIP() string {
 	return strings.TrimSpace(str)
 }
 
+//Read the IP from the file, "./public/json/ip", return it as a string
 func getIPFromFile() string {
 	content, err := ioutil.ReadFile("./public/json/ip")
 	if err != nil {
@@ -122,6 +135,7 @@ func getIPFromFile() string {
 	return lines[0]
 }
 
+//grab the email from "./public/json/email" to be used if the user has a dynamically assigned IP, and the IP changes from before
 func getEmail() string {
 	content, err := ioutil.ReadFile("./public/json/email")
 	if err != nil {
@@ -131,14 +145,17 @@ func getEmail() string {
 	return (lines[0])
 }
 
+//Function that checks to see if the current IP matches the IP string currently registered.
+//If the old IP and the new IP don't match, send the user an email notifying them of this change. Please change the stored at ./public/json/ip to get these notifications
 func send(body string) {
 	if body == IP {
 		return
 	} else {
 		IP = NewIP
 		writeIP(IP)
-		from := "prometheusclock@gmail.com"
-		pass := "abcprometheusclock"
+		//Account from which Prometheus sends an email from.
+		from := "email@example.com"
+		pass := "password"
 		var to string
 		to = getEmail()
 
@@ -160,6 +177,7 @@ func send(body string) {
 	}
 }
 
+//Sends the signal to turn on the bed vibrator by sending a High (true) signal to GPIO 17
 func VibOn() {
 	// fmt.Println("VibOn")
 	if err := rpio.Open(); err != nil {
@@ -178,6 +196,7 @@ func VibOn() {
 	Enable.High()
 }
 
+//Sends the signal to turn off the bed vibrator by sending a Low (false) signal to GPIO 17
 func VibOff() {
 	// fmt.Println("VibOff")
 	if err := rpio.Open(); err != nil {
@@ -196,6 +215,7 @@ func VibOff() {
 	Enable.Low()
 }
 
+//Get the alarms configurations from the ./public/json/alarms.json
 func getRawJson(filepath string) []jsonAlarms {
 	raw, err1 := ioutil.ReadFile(filepath)
 	if err1 != nil {
@@ -211,6 +231,7 @@ func getRawJson(filepath string) []jsonAlarms {
 	return alarm
 }
 
+//Populate the values of all 4 internally stored alarms using the values stored in the ./public/json/alarms.json
 func (argumentalarm *Alarm) initializeAlarms(jsondata []jsonAlarms, index int) {
 	argumentalarm.Name = string(jsondata[index].JsonName)
 	argumentalarm.Alarmtime = string(jsondata[index].JsonTime)
@@ -228,12 +249,14 @@ func (argumentalarm *Alarm) initializeAlarms(jsondata []jsonAlarms, index int) {
 
 }
 
+//General error handler. I guess it wasn't used nearly as much as should to warrant it's existance, but its here nonetheless.
 func Errhandler(err error) {
 	if err != nil {
 		fmt.Println("ERROR")
 	}
 }
 
+//Function that adds 10 minutes to the currently running alarm. Necessary because internally, alarm times are stored as strings, rather than the time class
 func (arg *Alarm) addTime(originaltime string, hms string, byhowmuch int) { //takes originaltime, and adds byhowmuch hours/minutes/seconds, then returns the string
 	currenttime, _ := time.Parse("15:04", originaltime)
 	//fmt.Println("before fixed snooze time", currenttime)
@@ -250,6 +273,8 @@ func (arg *Alarm) addTime(originaltime string, hms string, byhowmuch int) { //ta
 	//fmt.Println("fixed snooze time", arg.Alarmtime)
 }
 
+//Function to check whether the alarm has been running for more than 10 minutes
+//***NOT WORKING AS OF NOW, BUT DOES NOT NEGATIVELY AFFECT THE FUNCTIONALITY OF THE REST OF THE PROGRAM***
 func OverTenMinutes(alarmtime string) bool {
 	// fmt.Println("OverTenMinutes")
 	year, month, day := time.Now().Date()
@@ -281,6 +306,7 @@ func OverTenMinutes(alarmtime string) bool {
 	}
 }
 
+//Since the Sound and Vibration variables are stored as "on" or "off" in the alarms.json file, this function converts a boolean to the on/off format
 func convertBooltoString(arg bool) string {
 	if arg {
 		return "on"
@@ -289,6 +315,7 @@ func convertBooltoString(arg bool) string {
 	}
 }
 
+//Write back the correct alarm configurations to ./public/json/alarms.json so that the information can be retrieved when ./main is restarted
 func writeBackJson(Alarm1 Alarm, Alarm2 Alarm, Alarm3 Alarm, Alarm4 Alarm, filepath string) {
 	// fmt.Println("[{\"name\":\"" + Alarm1.Name + "\",\"time\":\"" + Alarm1.Alarmtime + "\",\"sound\":\"" + convertBooltoString(Alarm1.Sound) + "\",\"vibration\":\"" + convertBooltoString(Alarm1.Vibration) + "\"}\n{\"name\":\"" + Alarm2.Name + "\",\"time\":\"" + Alarm2.Alarmtime + "\",\"sound\":\"" + convertBooltoString(Alarm2.Sound) + "\",\"vibration\":\"" + convertBooltoString(Alarm2.Vibration) + "\"}\n{\"name\":\"" + Alarm3.Name + "\",\"time\":\"" + Alarm3.Alarmtime + "\",\"sound\":\"" + convertBooltoString(Alarm3.Sound) + "\",\"vibration\":\"" + convertBooltoString(Alarm3.Vibration) + "\"}\n{\"name\":\"" + Alarm4.Name + "\",\"time\":\"" + Alarm4.Alarmtime + "\",\"sound\":\"" + convertBooltoString(Alarm4.Sound) + "\",\"vibration\":\"" + convertBooltoString(Alarm4.Vibration) + "\"}]")
 	content := []byte("[{\"name\":\"" + Alarm1.Name + "\",\"time\":\"" + Alarm1.Alarmtime + "\",\"sound\":\"" + convertBooltoString(Alarm1.Sound) + "\",\"vibration\":\"" + convertBooltoString(Alarm1.Vibration) + "\"},\n{\"name\":\"" + Alarm2.Name + "\",\"time\":\"" + Alarm2.Alarmtime + "\",\"sound\":\"" + convertBooltoString(Alarm2.Sound) + "\",\"vibration\":\"" + convertBooltoString(Alarm2.Vibration) + "\"},\n{\"name\":\"" + Alarm3.Name + "\",\"time\":\"" + Alarm3.Alarmtime + "\",\"sound\":\"" + convertBooltoString(Alarm3.Sound) + "\",\"vibration\":\"" + convertBooltoString(Alarm3.Vibration) + "\"},\n{\"name\":\"" + Alarm4.Name + "\",\"time\":\"" + Alarm4.Alarmtime + "\",\"sound\":\"" + convertBooltoString(Alarm4.Sound) + "\",\"vibration\":\"" + convertBooltoString(Alarm4.Vibration) + "\"}]")
@@ -299,12 +326,15 @@ func writeBackJson(Alarm1 Alarm, Alarm2 Alarm, Alarm3 Alarm, Alarm4 Alarm, filep
 	}
 }
 
+//Handle the AJAX file upload from the client. Deletes the old file, and saves the new name of the sound from the header.Filename
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	//Delete the old alarm sound via shell command process rm public/assets/sound_name.extension
 	rmerror := exec.Command("rm", "public/assets/"+Soundname).Run()
 	if rmerror != nil {
 		fmt.Println(os.Stderr, rmerror)
 	}
 	file, header, err := r.FormFile("audio")
+	//Set the Soundname attribute to the new soundname
 	Soundname = header.Filename
 	//_, filename, err := r.FormFile("filename")
 	//fmt.Println(header.Filename)
@@ -316,6 +346,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
+	//Write the soundname to ./public/assets/sound_name.extension
 	out, err1 := os.Create("./public/assets/" + header.Filename)
 
 	//if err != nil {
@@ -334,21 +365,25 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, header.Filename)
 }
 
+//Initialization
 func init() {
+	//Save the JSON alarms configurations into the mold
 	jsondata := getRawJson("./public/json/alarms.json")
+	//Set up the intial values for the alarms using the values we get from above
 	Alarm1.initializeAlarms(jsondata, 0)
 	Alarm2.initializeAlarms(jsondata, 1)
 	Alarm3.initializeAlarms(jsondata, 2)
 	Alarm4.initializeAlarms(jsondata, 3)
-
+	//Read the most recent IP address from ./public/json/ip
 	IP = strings.TrimSpace(getIPFromFile())
-
+	//Grab the name of the alarm sound file via ls command to the ./public/assets/ folder.
 	var b bytes.Buffer
 	if err := Execute(&b,
 		exec.Command("ls", "public/assets"),
 	); err != nil {
 		log.Fatalln(err)
 	}
+	//Since the ls stdout returns the string + a newline (\n), the following strips the newline from the string, and saves it as the Soundname variable
 	Soundname = strings.TrimSpace(b.String())
 	//fmt.Println(Soundname)
 	d1 := []byte(Soundname)
@@ -364,6 +399,8 @@ func main() {
 	t := time.Now()
 	currenttime := t.Format("15:04")
 	c := cron.New()
+	//Run the following once a minute
+	//Check all 4 alarms to see if the current time matches any configurations
 	c.AddFunc("0 * * * * *", func() {
 		breaktime := false
 		duration := time.Second * 3
@@ -757,6 +794,7 @@ func main() {
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/", fs)
 
+	//Handle the AJAX post call to submit a new time for a certain alarm
 	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
 		erawr := r.ParseForm()
 		if erawr != nil {
@@ -766,6 +804,7 @@ func main() {
 		name := r.FormValue("name")
 		time := r.FormValue("value")
 		//fmt.Println(name)
+		//Check to see which alarm the user is actually trying to modify, and modify the correct internally stored time
 		if name == "alarm1" {
 			Alarm1.Alarmtime = time
 			Alarm1.CurrentlyRunning = false
@@ -779,6 +818,7 @@ func main() {
 			Alarm4.Alarmtime = time
 			Alarm4.CurrentlyRunning = false
 		}
+		//Write back the alarm data back to the ./public/json/alarms.json so Prometheus could retreive the data after a restart
 		writeBackJson(Alarm1, Alarm2, Alarm3, Alarm4, "./public/json/alarms.json")
 	})
 
@@ -798,6 +838,7 @@ func main() {
 			boolsound = false
 		}
 
+		//Check to see which alarm the user is actually trying to modify, and modify the correct internally stored sound
 		if name == "alarm1" {
 			Alarm1.Sound = boolsound
 			Alarm1.CurrentlyRunning = false
@@ -811,6 +852,7 @@ func main() {
 			Alarm4.Sound = boolsound
 			Alarm4.CurrentlyRunning = false
 		}
+		//Write back the alarm data back to the ./public/json/alarms.json so Prometheus could retreive the data after a restart
 		writeBackJson(Alarm1, Alarm2, Alarm3, Alarm4, "./public/json/alarms.json")
 	})
 
@@ -829,6 +871,7 @@ func main() {
 		} else {
 			boolvibration = false
 		}
+		//Check to see which alarm the user is actually trying to modify, and modify the correct internally stored vibration
 		if name == "alarm1" {
 			Alarm1.Vibration = boolvibration
 			Alarm1.CurrentlyRunning = false
@@ -842,11 +885,13 @@ func main() {
 			Alarm4.Vibration = boolvibration
 			Alarm4.CurrentlyRunning = false
 		}
+		//Write back the alarm data back to the ./public/json/alarms.json so Prometheus could retreive the data after a restart
 		writeBackJson(Alarm1, Alarm2, Alarm3, Alarm4, "./public/json/alarms.json")
 	})
 
 	http.HandleFunc("/snooze", func(w http.ResponseWriter, r *http.Request) {
 		//fmt.Println("snoozed")
+		//Using the addTime() function, add 10 minutes to the currently running alarm, turn off the currently running alarm, and write back the correct configuration back to ./public/json/alarms.json
 		if Alarm1.CurrentlyRunning {
 			Alarm1.CurrentlyRunning = false
 			Alarm1.addTime(Alarm1.Alarmtime, "m", 10)
@@ -867,6 +912,7 @@ func main() {
 		http.Redirect(w, r, "/", 301)
 	})
 
+	//Pass on the AJAX post /upload handler to the uploadHandler() function
 	http.HandleFunc("/upload", uploadHandler)
 	log.Println("Listening...")
 	log.Fatal(http.ListenAndServe(":3000", nil))
