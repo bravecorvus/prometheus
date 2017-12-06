88dab1979cb29eeeed3fbda9bedc6621a4be1638
Andrew Lee <leeas@stolaf.edu>
2017-12-06 01:08:50 -0600

    updated to fix issue with shairport-sync

--- utils/utils.go
+++ utils/utils.go
@@ -235,7 +235,7 @@ func CheckShairportSyncInstalled() bool {
 	if err != nil {
 		re := regexp.MustCompile("^Daemon already running on")
 		found := re.FindString(err.Error())
-		fmt.Println(re)
+		fmt.Println(found)
 		if found == "" {
 			return false
 		} else {
