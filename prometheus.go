3eb7ca41f50f68dca06b3c0ec759b29695076afe
gilgameshskytrooper <leeas@stolaf.edu>
2018-01-06 17:01:24 -0600

    update to use TLS encryption

--- prometheus.go
+++ prometheus.go
@@ -210,7 +210,6 @@ func main() {
 
 		if EnableLed {
 			if foundNixie {
-				fmt.Println(nixie.CurrentTimeAsString() + Red + Green + Blue)
 				b := []byte(nixie.CurrentTimeAsString() + Red + Green + Blue)
 				_, err := port.Write(b)
 				if err != nil {
@@ -1485,5 +1484,10 @@ func main() {
 	//Pass on the AJAX post /upload handler to the uploadHandler() function
 	http.HandleFunc("/upload", uploadHandler)
 	log.Println("Listening...")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	tlserr := http.ListenAndServeTLS(":3000", "server.crt", "server.key", nil)
+	if tlserr != nil {
+		fmt.Println("If you want the program to utilize TLS (i.e. host an encrypted HTTPS front end, please do the following in command line in the same directory as prometheus.go to first create a private self-signed rsa key, then a public key (x509) key based on the private key:\n\topenssl genrsa -out server.key 2048\n\topenssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650\nThen make sure you finish filling in the details asked in command line.\n\nFor now, unencrypted http will be used.")
+		log.Fatal(http.ListenAndServe(":3000", nil))
+	}
+
 }
