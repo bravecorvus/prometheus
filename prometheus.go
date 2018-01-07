18ff7615119dc904f1da91a7db5ba2b3aeea5ac5
gilgameshskytrooper <leeas@stolaf.edu>
2018-01-07 13:42:09 -0600

    small change

--- prometheus.go
+++ prometheus.go
@@ -1486,6 +1486,7 @@ func main() {
 	log.Println("Listening...")
 	if utils.Exists(utils.Pwd()+"/server.crt") && utils.Exists(utils.Pwd()+"/server.key") {
 		log.Fatal(http.ListenAndServeTLS(":3000", utils.Pwd()+"/server.crt", utils.Pwd()+"/server.key", nil))
+	} else {
 		fmt.Println("If you want the program to utilize TLS (i.e. host an encrypted HTTPS front end, please do the following in command line in the same directory as the bigdisk executable to first create a private self-signed rsa key, then a public key (x509) key based on the private key:\n\topenssl genrsa -out server.key 2048\n\topenssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650\nThen make sure you finish filling in the details asked in command line.\n\nFor now, unencrypted http will be used.")
 		log.Fatal(http.ListenAndServe(":3000", nil))
 	}
